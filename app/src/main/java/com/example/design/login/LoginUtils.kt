2a07fd6927166bcda373f5358b0cfb5086001c8a
Dmoeen <daniyal.moeen@gmail.com>
2020-11-25 18:42:13 +0500

    Design Project

--- app/src/main/java/com/example/design/login/LoginUtils.kt
+++ app/src/main/java/com/example/design/login/LoginUtils.kt
@@ -1,14 +1,15 @@
 package com.example.design.login
+
 import com.google.android.material.textfield.TextInputLayout
 
 object LoginUtils {
 
-    fun lookUpForUserName(userName:TextInputLayout):Boolean{
-       return true
+    fun lookUpForUserName(userName: TextInputLayout): Boolean {
+        return true
     }
 
 
-    fun passWordValidation(userName:TextInputLayout):Boolean{
+    fun passWordValidation(userName: TextInputLayout): Boolean {
         return true
     }
 }
